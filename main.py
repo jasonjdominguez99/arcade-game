ec2797a2bf91e383f7384117bec9542e2f7d4737
Jason Dominguez <jasonjdominguez99@gmail.com>
2021-10-14 16:15:54 +0100

    Created basic window using Tkinter

--- main.py
+++ main.py
@@ -6,11 +6,25 @@
 # Date: 14-10-2021
 
 # Imports
+from tkinter import Tk
+
+# Definition of utility functions
+def configure_window(window):
+    window.geometry("800x600")
+    window.configure(background='#b3ffff')
+    window.title("G A M E    T I T L E")
+
+# Started implementing main but ran out of time
+# come back to this
+# def game_loop():
+#     num1 = rand(1, 20)
 
 
 # Definition and execution of main program
 def main():
-    print("Hello, world!")
+    window = Tk()
+    configure_window(window)
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
