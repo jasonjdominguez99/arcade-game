7f84aeb5f76bf96f9c44b82af19f759697e2bc66
Jason Dominguez <jasonjdominguez99@gmail.com>
2021-10-14 15:29:03 +0100

    Initial commit, created main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+# Main
+# 
+# Main python file for python arcade-style game
+# 
+# Author: Jason Dominguez
+# Date: 14-10-2021
+
+# Imports
+
+
+# Definition and execution of main program
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
