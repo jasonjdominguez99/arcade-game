b22dd2cc0469a69d179985fd9901815d8361a9b7
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2022-05-28 16:55:44 +0100

    Started definitions of start menu functions

---
+++ start_menu.py
@@ -0,0 +1,25 @@
+# start_menu.py
+# 
+# source code relating to loading the
+# start menu for the arcade game
+# 
+# Author: Jason Dominguez
+# Date: 2022-05-28
+
+# Imports
+from tkinter import (
+    Tk, Button
+)
+
+# Function definitions
+def load_start_menu(window):
+    start_button = Button(window, )
+    pass
+
+# Definition and execution of main program
+def main():
+    window = Tk()
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
