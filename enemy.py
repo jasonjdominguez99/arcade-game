20fcdf12665c5b6ecb311533e56b6aacd93985be
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2022-06-23 12:27:40 +0100

    Started creating enemy classes

---
+++ enemy.py
@@ -0,0 +1,20 @@
+# enemy.py
+# 
+# source code relating to enemy objects, including
+# asteroids and alien space ships
+# 
+# author: jason dominguez
+# date: 2022-06-23
+
+
+# imports
+
+
+# class definitions
+# TODO: create enemy object base class
+# TODO: create asteroid class using inheritance
+# TODO: create alien ship class using inheritance
+class Enemy():
+    def __init__(self, canvas):
+        self.canvas = canvas
+        
